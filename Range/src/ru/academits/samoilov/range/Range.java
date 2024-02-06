74a4436e9ed30187f6fbe01ef1a93a49ab60ec03
Korgi <leonidacc@gmail.com>
2024-02-06 16:51:21 +0700

    Initial commit

---
+++ Range/src/ru/academits/samoilov/range/Range.java
@@ -0,0 +1,7 @@
+package ru.academits.samoilov.range;
+
+public class Range {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Range/src/ru/academits/samoilov/range/main/Main.java
@@ -0,0 +1,4 @@
+package ru.academits.samoilov.range.main;
+
+public class Main {
+}
